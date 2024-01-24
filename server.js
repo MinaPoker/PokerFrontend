077ebd7c725f676fb4e0d82c2d1179c9a2ec6987
0x <101751723+0xKryptonian@users.noreply.github.com>
2024-01-25 05:25:26 +0530

    setup the testing socket server

---
+++ server.js
@@ -0,0 +1,26 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+    createServer((req, res) => {
+        const parsedUrl = parse(req.url, true);
+        const { pathname, query } = parsedUrl;
+
+        if (pathname === '/create') {
+            console.log("socket running :", pathname)
+        } else if (pathname === '/game') {
+            console.log("socket running :", pathname)
+            app.render(req, res, '/b', query);
+        } else {
+            handle(req, res, parsedUrl);
+        }
+    }).listen(5005, (err) => {
+        if (err) throw err;
+        console.log('> Ready on http://localhost:5005');
+    });
+});
