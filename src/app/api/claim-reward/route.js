708009cfdd7ec798eba2b62bf627aee0268fdb45
Vishal <vishalpatil080502@gmail.com>
2024-03-15 20:11:50 +0530

    feat(api): claiming rewards & streaks calculation

---
+++ src/app/api/claim-reward/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import {
+  addReward,
+  getReward,
+  addStreaks,
+  getStreaks,
+} from "@/util/databaseFunctions";
+
+export async function POST(req, res) {
+  const { address } = await req.json();
+  const reward = await getReward(address);
+  const streak = await getStreaks(address);
+  var reward_claimed = reward[0].rewards;
+  var increment = 700 - streak[0].streaks * 100;
+  reward_claimed += increment;
+  const updateStreaks = await addStreaks(streak[0].streaks + 1);
+  const data = await addReward(address, reward_claimed);
+  return NextResponse.json({
+    reward: reward_claimed,
+    streak: streak[0].streaks + 1,
+    msg: "Reward claimed",
+  });
+}
